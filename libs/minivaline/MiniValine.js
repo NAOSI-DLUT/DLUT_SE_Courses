4aca19ae0705909f33388557dc12dd0a43b6ad2c
dingiso <luruibo2000@163.com>
2021-06-16 13:27:27 +0800

    Site updated: 2021-06-16 13:27:26

---
+++ libs/minivaline/MiniValine.js
@@ -0,0 +1,1 @@
+function loadScript(src,cb){var HEAD=document.getElementsByTagName('head')[0]||document.documentElement;var script=document.createElement('script');script.setAttribute('type','text/javascript');if(cb)script.onload=cb;script.setAttribute('src',src);HEAD.appendChild(script)};loadScript('https://cdn.jsdelivr.net/npm/minivaline@latest')
